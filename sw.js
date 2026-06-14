80e0cd419abd70ccfd26b899c9a43586503fe11b
siscap-luccianos <gabrielbusquets86@gmail.com>
2026-06-14 14:09:56 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-// SW limpiador — borra todo y se desregistra
+// SW desactivado — borra todo el caché
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.matchAll())
-      .then(clients => clients.forEach(c => c.postMessage('reload')))
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
