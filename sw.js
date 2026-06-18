01d546c080a222753ea1c6e6782a0beee2d9e680
siscap-luccianos <gabrielbusquets86@gmail.com>
2026-06-17 23:28:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-// SW desactivado — borra todo el caché
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll().then(cs => cs.forEach(c => c.navigate(c.url))))
+  );
 });
